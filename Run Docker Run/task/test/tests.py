c5af63d9459f59dcf6d63c52b02c6cff40da1736
Miodrag <3128205+modulardata@users.noreply.github.com>
2024-04-29 06:39:57 +0200

    Stage 4/5

--- Run Docker Run/task/test/tests.py
+++ Run Docker Run/task/test/tests.py
@@ -1,11 +1,13 @@
-from pathlib import Path
-
 import docker
+import requests
 from hstest import StageTest, dynamic_test, CheckResult
 
-stage = Path(__file__).parent.parent
-
-project_images = ["python:3.11-slim", "hyper-web-app"]
+ancestor = "hyper-web-app"
+url = "http://localhost:8000"
+server_response = {
+    "first_name": "Hyper",
+    "id": "90", "last_name": "Skill"
+}
 
 
 class DockerTest(StageTest):
@@ -17,17 +19,40 @@ class DockerTest(StageTest):
 
     @dynamic_test()
     def test1(self):
-        print('DockerRootDir ' + self.client.info().get('DockerRootDir'))
-        images_docker = self.client.images.list(all=True)
-        for image in images_docker:
-            print('images in the docker ' + str(image.tags))
-        """Tests if image exists in the system"""
-        images_text = " ".join([str(image) for image in self.client.images.list()])
-        print('images_text ' + images_text)
-        for image in project_images:
-            if image not in images_text:
-                return CheckResult.wrong(f"'{image}' not found in the system images!")
+        """Tests image of container, state and exposed port"""
+        all_containers = self.client.containers.list(all=True)
+        for container in all_containers:
+            if container.attrs.get("Config").get("Image") == ancestor:
+                if container.attrs.get("State").get("Running"):
+                    if "8000" in str(container.attrs.get("Config").get("ExposedPorts").keys()):
+                        return CheckResult.correct()
+                    else:
+                        return CheckResult.wrong("The exposed port should be 8000!")
+                else:
+                    return CheckResult.wrong("The container should be running!")
 
+        return CheckResult.wrong(f"Couldn't find a container from the '{ancestor}' ancestor!")
+
+    @dynamic_test()
+    def test2(self):
+        """Tests if the webserver is running as expected"""
+        try:
+            response = requests.get(url)
+        except requests.exceptions.ConnectionError:
+            return CheckResult.wrong("Connection error!")
+        except requests.exceptions.InvalidURL:
+            return CheckResult.wrong("Couldn't connect to the server. Check if hyper-web-app is running as expected!")
+        except requests.exceptions.Timeout:
+            return CheckResult.wrong("Connection timed out, while trying to connect hyper-web-app!")
+        else:
+            if response.status_code != 200:
+                return CheckResult.wrong("Wrong status code returned from the hyper-web-app!")
+            try:
+                json_response = response.json()
+            except requests.exceptions.JSONDecodeError:
+                return CheckResult.wrong("Couldn't convert server response to dictionary!")
+            if json_response != server_response:
+                return CheckResult.wrong("Wrong content returned from the hyper-web-app!")
         return CheckResult.correct()
 
 
